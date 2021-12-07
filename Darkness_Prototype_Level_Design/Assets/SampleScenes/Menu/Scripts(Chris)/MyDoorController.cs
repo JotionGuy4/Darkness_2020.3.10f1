5cc332255a40c0462fcf0b1c0d2dbb036778ec88
Camilla <camille.ragland@gmail.com>
2021-12-07 18:45:38 -0500

    door changes

--- Darkness_Prototype_Level_Design/Assets/SampleScenes/Menu/Scripts(Chris)/MyDoorController.cs
+++ Darkness_Prototype_Level_Design/Assets/SampleScenes/Menu/Scripts(Chris)/MyDoorController.cs
@@ -10,46 +10,21 @@ public class MyDoorController : MonoBehaviour
     private void Awake()
     {
         DoorAnim = gameObject.GetComponent<Animator>();
-        checkInv = GetComponent<Inventory>();
-        noKnifeText.SetActive(false);
-        carriesKnifeText.SetActive(false);
-    }
-
-    void OnTriggerEnter(Collider col)
-    {
-        if (col.tag == "InteractiveObject")
-        {
-            if (checkInv.hasknife == true)
-            {
-                carriesKnifeText.SetActive(true);
-                DoorOpen = true;
-                PlayAnimation();
-            }
-            else
-            {
-                noKnifeText.SetActive(true);
-                DoorOpen = false;
-            }
-        }
-        else
-        {
-            noKnifeText.SetActive(false);
-            carriesKnifeText.SetActive(false);
-        }
     }
 
         public void PlayAnimation()
-    {
-        if (DoorOpen == true)
         {
-            DoorAnim.Play("DoorOpen", 0, 0.0f);
-        }
-        else
-        {
-            DoorAnim.Play("DoorClose", 0, 0.0f);
-            DoorOpen = false;
+            if (!DoorOpen)
+            {
+                DoorAnim.Play("DoorOpen", 0, 0.0f);
+                DoorOpen = true;
+            }
+            else
+            {
+                DoorAnim.Play("DoorClose", 0, 0.0f);
+                DoorOpen = false;
 
+            }
         }
-    }
 
 }
